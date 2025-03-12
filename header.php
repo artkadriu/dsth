9d807ebe2b3fb9319cce144057ce1228f2339fc0
AmarBytyqi <amar.bytyci@gmail.com>
2025-03-12 16:51:07 +0100

    3e

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<header>
+    <section class="top-bar">
+        <div class="logo">
+            Logo
+        </div>
+        <div class="searchbox">
+            Search Box
+        </div>
+    </section>    
+    <section class="menu-area">
+        <nav class="main-menu">
+            <?php wp_nav_menu(
+                array('theme_location' => 'wp_devs_main_menu', 'depth' => 2));
+             ?>
+        </nav>
+    </section>
+</header>
--- index.php
+++ index.php
@@ -1,29 +1,7 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset=<?php bloginfo('charset')?>>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DS Theme</title>
-</head>
-<body <?php body_class()?>>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, nam.</p>
-<div id="page" class="site">
-    <header>
-        <section class="top-bar">
-            <div class="logo">
-                Logo
-            </div>
-            <div class="search-box">
-                Search
-            </div>
-        </section>
-        <section class="menu-area">
-            <nav class="main-menu">
-                Menu
-            </nav>
-        </section>
-    </header>
-<!--header-->
+<?php get_header();  ?>
+
+<!---->
+
 <div id="content" class="site-content">
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
@@ -39,10 +17,9 @@
         </main>
     </div>
 </div>
-<!--body-->
-<footer class="site-footer">
-    Footer
-</footer>
-</div>
-</body>
-</html>
+<!---->
+<?php get_footer(); ?>
+
+
+
+
